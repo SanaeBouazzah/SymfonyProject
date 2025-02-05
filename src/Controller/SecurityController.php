6e569c9f1d62c9f1ca50dc23508cd5b0d4f79ef1
Sanae Bouazzah <sanaebouazzah2000@gmail.com>
2025-02-05 15:35:37 +0100

    Update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,11 +11,13 @@ use Symfony\Component\Security\Core\Security;
 class SecurityController extends AbstractController
 {  
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response{
-        if ($this->getUser()) return $this->redirectToRoute('app_login');
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    public function login(AuthenticationUtils $authenticationUtils): Response {
+      if ($this->getUser()) {
+          return $this->redirectToRoute('app_dashboard'); 
+      }
+      $error = $authenticationUtils->getLastAuthenticationError();
+      $lastUsername = $authenticationUtils->getLastUsername();
+      return $this->render('security/login.html.twig', [ 'last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
